face19b4a8715ee87b48ea8de263d06e54869aa7
sarahduv <46334082+sarahduv@users.noreply.github.com>
2020-02-18 16:53:20 -0800

    weapons funtionality complete

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,16 +9,84 @@ public class Player : MonoBehaviour
     private float _speed = 3.5f;
     [SerializeField]
     private float _gravity = 9.81f;
+    [SerializeField]
+    private GameObject _muzzleFlash;
+    [SerializeField]
+    private GameObject _hitMarkerPrefab;
+    [SerializeField]
+    private AudioSource _weaponAudio;
+    [SerializeField]
+    private int _currentAmmo;
+    private int _maxAmmo = 50;
+    private bool _isReloading = false;
+
+    private UIManager _uiManager;
+
     // Start is called before the first frame update
     void Start()
     {
         _controller = GetComponent<CharacterController>();
+        _uiManager = GameObject.Find("Canvas").GetComponent<UIManager>();
+
+
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        _currentAmmo = _maxAmmo;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetMouseButton(0) && _currentAmmo > 0)
+        {
+            Shoot();
+        }
+        else
+        {
+            _muzzleFlash.SetActive(false);
+            _weaponAudio.Stop();
+        }
+
+        if (Input.GetKeyDown(KeyCode.R) && !_isReloading)
+        {
+            _isReloading = true;
+            StartCoroutine(Reload());
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
         calculateMovement();
+    }
+
+    void Shoot()
+    {
+        _muzzleFlash.SetActive(true);
+        _currentAmmo--;
+        _uiManager.UpdateAmmo(_currentAmmo);
+
+        if (!_weaponAudio.isPlaying)
+        {
+            _weaponAudio.Play();
+
+        }
+
+        // 0.5F to get the middle of the viewport - which is where the crosshair is
+        Ray rayOrigin = Camera.main.ViewportPointToRay(new Vector3(0.5F, 0.5F, 0));
+        RaycastHit hitInfo;
+
+        // hit input will be output --> whatever the raycast hit
+        if (Physics.Raycast(rayOrigin, out hitInfo))
+        {
+            Debug.Log("Hit: " + hitInfo.transform.name);
+            // we want the prefab effect to be from the perpendicular normal
+            // IE: if it's a person standing, the effect needs to come facing out
+            // not facing up. The Instantiated object needs to be type casted as GameObject
+            GameObject hitMarker = Instantiate(_hitMarkerPrefab, hitInfo.point, Quaternion.LookRotation(hitInfo.normal)) as GameObject;
+            Destroy(hitMarker, 1f);
+        }
     }
 
     void calculateMovement()
@@ -31,5 +99,13 @@ public class Player : MonoBehaviour
         // world space values instead of local
         velocity = transform.transform.TransformDirection(velocity);
         _controller.Move(velocity * Time.deltaTime);
+    }
+
+    IEnumerator Reload()
+    {
+        yield return new WaitForSeconds(1.5f);
+        _currentAmmo = _maxAmmo;
+        _uiManager.UpdateAmmo(_currentAmmo);
+        _isReloading = false;
     }
 }
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField]
+    private Text _ammoText;
+
+    public void UpdateAmmo(int count)
+    {
+        _ammoText.text = "Ammo: " + count.ToString();
+    }
+}
